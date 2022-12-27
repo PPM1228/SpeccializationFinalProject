02f068026c6a5c67e6019ce1498df1b7365b2c82
Philipp <philer7@gmail.com>
2022-12-27 21:54:17 +0700

    Составлена схема решения задачи

--- Program.cs
+++ Program.cs
@@ -8,26 +8,3 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
 
-
-
-
-// void ThreeCharWordInArray (string[]array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         int array_size = 0;
-//         for (int j = 0; j < array[i].Length; j++)
-//             {
-//                 array_size = array_size + 1;
-//             }
-//         {
-//             if (array_size <= 3)
-//             {
-//                 Console.WriteLine($"The word consisting of 3 chars is {array[i]}");
-//             }
-//         }
-//     }
-// }
-
-// string[] new_array = {Hello, 2, world, :-)”};
-// ThreeCharWordInArray(new_array);
